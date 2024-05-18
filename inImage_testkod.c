8220c36418dc215cc1d97e51d1a8a40640421404
samuelek <ek.samuel@icloud.com>
2024-05-18 18:07:17 +0200

    Update files

--- inImage_testkod.c
+++ inImage_testkod.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
-extern void inImage();
-extern char input_buffer[256];
-extern int buffer_position;
-
-int main() {
-    inImage();  // Anropa inImage för att läsa in data
-    printf("Input buffer: %s\n", input_buffer);  // Skriv ut innehållet i input_buffer
-    printf("Buffer position: %d\n", buffer_position);  // Kontrollera att positionen är nollställd
-    return 0;
+extern int MaxOfThree(int, int, int);
+int main()
+{
+printf("Maxvärdet av talen 1, -4 och -7 är %d\n", MaxOfThree(1,-4,-7));
+printf("Maxvärdet av talen 2, -6 och 1 är %d\n", MaxOfThree(2, -6, 1));
+printf("Maxvärdet av talen 2, 3 och 1 är %d\n", MaxOfThree(2, 3, 1));
+printf("Maxvärdet av talen -2, 4 och 3 är %d\n", MaxOfThree(-2, 4, 3));
+printf("Maxvärdet av talen 2, -6 och 5 är %d\n", MaxOfThree(2, -6, 5));
+printf("Maxvärdet av talen 2, 4 och 6 är %d\n", MaxOfThree(2, 4, 6));
+return 0;
 }
