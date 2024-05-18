881ad870b1cee5acad45da17ffdad8f5db4ad62e
Oscar Lövkvist Larsson <oscar.lofqvistlarsson.com>
2024-05-18 22:14:51 +0200

    inimage och putext med kommentarer

--- inImage.s
+++ inImage.s
@@ -2,23 +2,73 @@
 # Fgets har parametrar (buffert, antal, fil)
 # stdin -> Tangentbordet
 
-.data 
-buffer:
-    .space 1024 # Reservera plats för inmatningsbufferten (1024 bytes, kan väl ändra om man vill??)
-buffer_pointer:
-    .byte 0
+.section .data
+in_buffer:
+    .space 1024 # Reservera inmatningsbufferten
+in_buffer_pos:
+    .quad 0
 
-.text
-.global inImage 
+out_buffer:
+    .space 1024 # Reservera utmatningsbuffert
+out_buffer_pos:
+    .quad 0
+
+.section .text
+.global inImage
 inImage:
-    movq $buffer, %rdi
-    movq $5, %rsi
-    movq stdin, %rdx
+    pushq %rbp # pushar register ägda av den caller
+    movq %rsp, %rbp # lägg registerna på stacken
+
+    movq $in_buffer, %rdi # buffer address
+    movq $1024, %rsi # buffer strl
+    movq $0, %rdx # stdin
+
     call fgets
 
-    # Nollställa buffer_pointer
-    movb $0, buffer_pointer
+    # Nollställa buffer position
+    movb $0, in_buffer_pos
 
+    /*återställ*/
+    movq %rbp, %rsp
+    popq %rbp
     ret
 
+.global outImage:
+    pushq %rbp # pushar register ägda av den caller
+    movq %rsp, %rbp # lägg registerna på stacken
+
+    movq $out_buffer, %rdi # buffer address
+
+    call puts
+
+    # Nollställa buffer position
+    movq $0, out_buffer_pos
+
+    /*återställ*/
+    movq %rbp, %rsp
+    popq %rbp
+    ret
+
+.global putText
+putText:
+    pushq %rbp # pushar register ägda av den caller
+    movq %rsp, %rbp # lägg registerna på stacken
+
+    movq $out_buffer, %rdi # buffer address
+    movq %rsi, %rsi # ladda in värde
+
+loop_in_putText:
+    movb (%rsi), %al # läser en byte från text
+    movb %al, out_buffer(%rdi) # kopiera byten till buffern
+    incq %rdi # öka addressen
+    incq %rsi # ökar till nästa byte i texten
+    cmpb $0, %al # kolla om vi nått slutet av filen/texten
+    jne putText_loop # om inte loopa
+
+    movq %rdi, out_buffer_pos  # om vi nått uppdatera addressen
+
+    /*återställ*/
+    movq %rbp, %rsp
+    popq %rbp
+    ret
 
