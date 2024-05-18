ac8d8a8e97b4005b44805ebd837a9515629dab5d
samuelek <ek.samuel@icloud.com>
2024-05-18 17:19:28 +0200

    Added a DIR

---
+++ Rutiner/C-testkoder/inImage_testkod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern void inImage();
+extern char input_buffer[256];
+extern int buffer_position;
+
+int main() {
+    inImage();  // Anropa inImage för att läsa in data
+    printf("Input buffer: %s\n", input_buffer);  // Skriv ut innehållet i input_buffer
+    printf("Buffer position: %d\n", buffer_position);  // Kontrollera att positionen är nollställd
+    return 0;
+}
